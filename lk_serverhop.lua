ddb9d4eb2afa5f69a2a204e4bd4f10d81368daac
Zipettte <99619547+Zipettte@users.noreply.github.com>
2024-08-20 10:05:26 +0200

    Update lk_serverhop.lua

--- lk_serverhop.lua
+++ lk_serverhop.lua
@@ -1,15 +1,28 @@
 local ServerHop = {}
 
--- Fonction pour trouver un autre serveur avec le moins de joueurs possible
 function ServerHop:Teleport(placeId)
-    local servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"2753915549"/servers/Public?sortOrder=Asc&limit=100"))
+    -- Obtenir la liste des serveurs pour le jeu avec placeId
+    local success, response = pcall(function()
+        return game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. placeId .. "/servers/Public?sortOrder=Asc&limit=100"))
+    end)
+    
+    if not success then
+        warn("Erreur lors de la récupération des serveurs : " .. tostring(response))
+        return
+    end
+    
+    local servers = response.data
 
-    for _, server in pairs(servers.data) do
+    for _, server in pairs(servers) do
         if server.playing < server.maxPlayers then
-            game:GetService("TeleportService"):TeleportToPlaceInstance(placeId, server.id)
-            break
+            local teleportService = game:GetService("TeleportService")
+            -- Tente de téléporter à l'instance du serveur
+            teleportService:TeleportToPlaceInstance(placeId, server.id)
+            return
         end
     end
+    
+    warn("Aucun serveur disponible trouvé.")
 end
 
 return ServerHop
